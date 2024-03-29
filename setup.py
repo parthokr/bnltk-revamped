ac2860a68923146ced987c4fac2bfd1dfd798f60
parthokr <partho.rajvor2016@gmail.com>
2024-03-30 02:26:14 +0600

    Fixed README.md path

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with codecs.open(os.path.join(here, 'bnltk', 'README.md'), encoding='utf-8') as f:
+with codecs.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 VERSION = '0.1.0'
